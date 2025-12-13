164548c7fd29d18cfdf29c0d2263bbe4fac04b27
Obeyroon <saberpotatoe@gmail.com>
2025-12-13 02:05:44 +0100

    B: Report - print report

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from stats import get_num_words, get_num_each_char
+from stats import get_num_words, get_num_each_char, sorted_list_num_char
 
 def main():
 
     book_path = "books/frankenstein.txt"
+    c_var = get_num_each_char(book_path)
 
     print("Found " + get_num_words(book_path) + " total words")
 
-    print(get_num_each_char(book_path))
+    #print(get_num_each_char(book_path))
+
+    print(sorted_list_num_char(c_var))
 
 
 main()
--- stats.py
+++ stats.py
@@ -12,12 +12,8 @@ def get_num_each_char(filepath):
     num_char = {}
 
     with open(filepath) as f:
-
         file_txt = f.read().lower()
         each_char = list(file_txt)
-
-        #for i in range(0, each_char):
-        
         for i in each_char:
             if i not in num_char:
                 num_char[i] = 1
@@ -25,3 +21,26 @@ def get_num_each_char(filepath):
                 num_char[i] += 1
 
     return num_char
+
+def sort_on(items):
+    return items["num"]
+
+def sorted_list_num_char(char_counts):
+
+    final = []
+    char_list = []
+    char_key, num_key = "char", "num"
+
+    for i in char_counts:
+        char = {char_key : i, num_key : char_counts[i]}
+        
+        if i.isalpha():
+            char_list.append(char)
+        
+        
+    char_list.sort(reverse = True, key=sort_on)
+
+    for i in char_list:
+        final.append(i["char"] + ": " + str(i["num"]))
+    
+    return final
