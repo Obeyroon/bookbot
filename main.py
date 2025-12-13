45c3300d71255f718810b53217020e936a232b3c
Obeyroon <saberpotatoe@gmail.com>
2025-12-13 14:23:43 +0100

    C: Report - end

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from stats import get_num_words, get_num_each_char, sorted_list_num_char
+import sys
 
 def main():
 
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) <= 1: 
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
     c_var = get_num_each_char(book_path)
 
     print("Found " + get_num_words(book_path) + " total words")
-
-    #print(get_num_each_char(book_path))
 
     print(sorted_list_num_char(c_var))
 
